12ed99003747ed3fb279e2aa34027c198ec88b5a
wadad zein <wadad_zein@hotmail.com>
2021-04-29 17:49:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+
 const testVar = {}
 
 function testFunc() {
   return "hi"
+}
+
+function superbowlWin(superbowls){
+  let win = superbowls.find(superbowl => superbowl.result === "W")
+  return win ? win.year : undefined;
 }
